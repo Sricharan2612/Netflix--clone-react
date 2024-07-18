db17e08aef0f1a73bd73fef5430cfcde9b7b8236
Sricharan2612 <sricharany2000@gmail.com>
2024-07-18 15:43:46 +0530

    completed design page and made it responsive

--- src/pages/DetailsPage/DetailsPage.jsx
+++ src/pages/DetailsPage/DetailsPage.jsx
@@ -1,10 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import './DetailsPage.css';
+import axios from '../../axios';
+import { useSelector } from 'react-redux';
+import { API_KEY } from '../../requests';
 
 const DetailsPage = () => {
-    return (
-        <div className='detailspage'>
+    const { movie } = useSelector(data => data.movie);
+    //States
+    const [video, setVideo] = useState(null);
 
-        </div>
+    const type = movie.name ? 'tv' : 'movie';
+    //useEffect
+    useEffect(() => {
+        const fetchVideo = async () => {
+            const response = await axios.get(`/${type}/${movie.id}/videos?api_key=${API_KEY}&language=en-US`);
+            setVideo(response.data.results[0]);
+        };
+        fetchVideo();
+    }, []);
+
+    return (
+        video && (
+            <div className='detailspage' style={{
+                // background: `url("https://image.tmdb.org/t/p/original/${movie.backdrop_path}")`,
+            }}>
+                <div className="details_body">
+                    <iframe src={`https://www.youtube.com/embed/${video.key}`} frameBorder="0"></iframe>
+                    <div className="details_content">
+                        <h1>{type === 'tv' ? movie.name : movie.title}</h1>
+                        <div className="details_info">
+                            <h3><span>Release Date:</span> {type === 'movie' ? movie.release_date : movie.first_air_date}</h3>
+                            <h3><span>Rating:</span> {movie.vote_average.toFixed(1)}</h3>
+                            <h3><span>Language:</span> {movie.original_language.toUpperCase()}</h3>
+                        </div>
+                        <h3>Overview:</h3>
+                        <p>{movie.overview}</p>
+                    </div>
+                </div>
+            </div >
+        )
     );
 };
 
--- src/requests.js
+++ src/requests.js
@@ -1,4 +1,4 @@
-const API_KEY = process.env.REACT_APP_API_KEY;
+export const API_KEY = process.env.REACT_APP_API_KEY;
 
 console.log(process.env.REACT_APP_API_KEY);
 const requests = {
